dedf83e2f6732056ba4c78150c13d0ef933fef48
Jeremy <jeremy.worden@gmail.com>
2024-12-03 11:38:22 -0800

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "@/hooks/use-toast"
+import Creatable, { useCreatable } from 'react-select/creatable';
 
 const Home = () => {
   const { toast } = useToast()
@@ -38,7 +39,7 @@ const Home = () => {
 
   return (
     <div className="min-h-screen p-10 bg-gray-100 dark:bg-black">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-2">React, Express and Tailwind App</h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-2">Cisco Unity Greetings TTS</h1>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         Boilerplate code for a full-stack application using React, Express and Tailwind CSS. This project is built with Vite, a modern build tool that is blazing fast and supports React, Vue, Svelte and vanilla JavaScript. The backend is a simple Express server that serves data to the frontend. The frontend is a React app that uses Tailwind CSS for styling. The project is set up with ESLint, Prettier and Stylelint for code quality. The project also includes a dark mode toggle and a toast notification system.
       </p>
